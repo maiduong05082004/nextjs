9ae461b74e2cefbd8e16b4c7938237414857e35e
maiduong <duongmdph40323@gmail.com>
2024-03-02 15:00:34 +0700

    router definition

---
+++ src/app/admin/add_product/page.tsx
@@ -0,0 +1,6 @@
+const add_product = () =>{
+    return (
+        <div>Thêm sản phẩm</div>
+    )
+}
+export default add_product;
---
+++ src/app/admin/edit_product/page.tsx
@@ -0,0 +1,6 @@
+const edit_product = () =>{
+    return (
+        <div>Sửa sản phẩm</div>
+    )
+}
+export default edit_product;
