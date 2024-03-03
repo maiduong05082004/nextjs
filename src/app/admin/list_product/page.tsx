a3fb5ce5bff54917949737ff4f79fd6b78b27b8a
maiduong <duongmdph40323@gmail.com>
2024-03-03 16:37:40 +0700

    cài đặt react bootstrap

--- src/app/admin/list_product/page.tsx
+++ src/app/admin/list_product/page.tsx
@@ -1,9 +1,14 @@
+'use client'
 import Link from "next/link";
+import { Button } from 'react-bootstrap';
+import style1 from '@/styles/app.module.css';
+import style2 from '@/styles/list_product.module.css';
 const list_product = () =>{
     return (
     <div>
         <div>Hiển thị sản phẩm</div>
-        <Link href={"/"}>Quay về trang chủ</Link>
+        <Button variant="primary">Thêm sản phẩm</Button>
+        <Link className={style1['red']} href={"/"}>Quay về trang chủ</Link>
     </div>
     );
 }
